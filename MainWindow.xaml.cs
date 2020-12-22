05c896d8b44ab59b474cba4e8703da49ac62ef03
Adrian Edelen <adrianedelen@gmail.com>
2020-12-22 11:48:24 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using System.Windows;
+
+using System.Windows.Data;
+
+using Microsoft.Win32;
+
+
+namespace AutoneticsPeakAndValley
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Action EmptyDelegate = delegate () { };
+        public OpenFileDialog fileDialog { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        //on click -
+        // open the file dialog box
+        //parse the data (most of this operation is similar to the console version)
+        //output the data to the datagrid
+        //output the data to the graph
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".csv"; // Required file extension 
+            fileDialog.Filter = "CSV Files|*.csv";
+            fileDialog.ShowDialog();
+
+            if (fileDialog.FileName.EndsWith(".csv"))
+            {
+                //var fileName = fileDialog.FileName;
+                fileBox.Text = fileDialog.FileName;
+                try
+                {
+                    Stream fileStream = fileDialog.OpenFile();
+                    //var path = "C:/Users/adria/source/repos/AutoneticsChallenge/Files/Code Test Eight Round.csv"; //insert the file path here
+                    // call the parse points method
+                    Point[] filePoints = ParsePoints(fileStream).ToArray();
+
+                    //Instantiate a PeakAndValleyFinder object, gibing it our array of point objects
+                    PeakAndValleyFinder peakAndValleyFinder = new PeakAndValleyFinder(filePoints);
+
+                    //call the DeterminePeaks and DetermineValleys methods (see definition in PeakAndValleyFinder.cs)
+                    IEnumerable<Point> peaks = peakAndValleyFinder.DeterminePeaks();
+                    IEnumerable<Point> valleys = peakAndValleyFinder.DetermineValleys();
+
+                    // concat and order the two Ienumerables and ToList them for console output
+                    var peaksAndValleys = peaks.Concat(valleys).OrderBy(s => s.X).ToList();
+                    //set properties for datagrid.
+                    CollectionViewSource itemCollectionViewSource;
+                    itemCollectionViewSource = (CollectionViewSource)(FindResource("ItemCollectionViewSource"));
+                    itemCollectionViewSource.Source = peaksAndValleys;
+                    //get the size of the arrays for the chart, scottplot needs doubles so i can't just use my List.
+                    var arraySize = peaksAndValleys.Count();
+                    //initialize some doubles for the graph
+                    double[] dataX = new double[arraySize];
+                    double[] dataY = new double[arraySize];
+                    //loop over adding the values to the arrays for the graph.
+                    for (int i = 0; i < arraySize; i++)
+                    {
+                        dataX[i] = peaksAndValleys[i].X;
+                        dataY[i] = peaksAndValleys[i].Y;
+                    }
+                    wpfPlot1.plt.PlotScatter(dataX, dataY);
+                    wpfPlot1.Render();
+                    fileStream.Close();
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex);
+                    fileBox.Text = "File is open in another program. Please close any other copies of the file and try again";
+                }
+            }
+        }
+        //Parse the dataset
+        private static IEnumerable<Point> ParsePoints(Stream file)
+        {
+            //instantiate a StreamReader to read the lines of the CSV
+            using (StreamReader reader = new StreamReader(file))
+            {
+                
+                //keep going until we reach the end
+                while (!reader.EndOfStream)
+                {
+                    
+                    //in each loop, we read the next line, and instantiate a new point object
+                    var dataLine = reader.ReadLine();
+                    var isNumber = false;
+                    
+                    //here we determine when the actual numbers start in the csv by reading lines and trying to parse them as doubles
+                    //if the parse succeeds we can continue on.
+                    while (!isNumber)
+                    {
+                        try
+                        {
+                            double.Parse(dataLine.Split(',')[0]);
+                            isNumber = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            dataLine = reader.ReadLine();
+                        }
+                    }
+                    
+                    Point point = new Point()
+                    {
+                        //split the two colums parse the strings into doubles and set the props of the point object
+                        X = double.Parse(dataLine.Split(',')[0]),
+                        Y = double.Parse(dataLine.Split(',')[1])
+                    };
+                    //return the point object
+                    yield return point;
+                }
+            }
+        }
+    }
+}
---
+++ PeakAndValleyFinder.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoneticsPeakAndValley
+{
+    class PeakAndValleyFinder
+    {
+        private Point[] AllPoints { get; set; }
+        private List<Point> Peaks { get; set; }
+        private List<Point> Valleys { get; set; }
+        private int CurrentPeroidIndex { get; set; }
+
+
+        public PeakAndValleyFinder(Point[] allPoints)
+        {
+            AllPoints = allPoints;
+        }
+
+        //determine Peaks and Determine Valleys checks that if the method has been run already
+        //and returns the already determined value if it is not null
+        //this allows you to call the method again if needed without doing all the calculations again
+        public IEnumerable<Point> DeterminePeaks()
+        {
+            if (Peaks == null)
+            {
+                FindPeaksAndValleys();
+            }
+
+            return Peaks;
+        }
+
+        public IEnumerable<Point> DetermineValleys()
+        {
+            if (Valleys == null)
+            {
+                FindPeaksAndValleys();
+            }
+
+            return Valleys;
+        }
+
+
+        private void FindPeaksAndValleys()
+        {
+            // initialize peaks and valleys
+            CurrentPeroidIndex = 0;
+            Peaks = new List<Point>();
+            Valleys = new List<Point>();
+
+            //isFindingPeak determines if we are starting with a trend towards a peak or valley, 
+            //this sets what the first thing we are looking for is peak/valley
+            bool isFindingPeak = AllPoints[0].Y < AllPoints[1].Y;
+            //the if/else here sets the first datapoint as a peak or valley based on the trend of the first two datapoints
+            if (isFindingPeak)
+            {
+                Valleys.Add(AllPoints[0]);
+            }
+            else
+            {
+                Peaks.Add(AllPoints[0]);
+            }
+            //we loop over the datapoints, fliiping between finding a peak and a valley as we find each
+            while (CurrentPeroidIndex < AllPoints.Length - 1)
+            {
+                if (isFindingPeak)
+                {
+
+                    Peaks.Add(FindNextPeak());
+                    isFindingPeak = false;
+                }
+                else
+                {
+                    Valleys.Add(FindNextValley());
+                    isFindingPeak = true;
+                }
+            }
+        }
+
+
+        private Point FindNextPeak()
+        {
+            //currentMax is intermediate, this changes as we iterate until we have found our next peak
+            //to the find the peak, we iterate over the data, setting the max value, until we find
+            //that the data starts to go the other way, at which point we know that we are moving towards
+            //the next valley
+            Point currentMax = AllPoints[CurrentPeroidIndex];
+            for (int i = CurrentPeroidIndex; i < AllPoints.Length; i++)
+            {
+                CurrentPeroidIndex = i;
+                if (currentMax.Y > AllPoints[i].Y)
+                {
+                    break;
+                }
+                else
+                {
+                    currentMax = AllPoints[i];
+                }
+            }
+            return currentMax;
+        }
+        //this method acts the same as FindNextPeak
+        private Point FindNextValley()
+        {
+            Point currentMin = AllPoints[CurrentPeroidIndex];
+            for (int i = CurrentPeroidIndex; i < AllPoints.Length; i++)
+            {
+                CurrentPeroidIndex = i;
+                if (currentMin.Y < AllPoints[i].Y)
+                {
+                    break;
+                }
+                else
+                {
+                    currentMin = AllPoints[i];
+                }
+            }
+            return currentMin;
+        }
+    }
+}
+
